994d433da51609f326a7b42eb6287a68948e7162
DevilXD <bartosz.peszek@gmail.com>
2021-02-09 19:55:31 +0100

    Average the activation function comparison over 10 passes

--- activation_compare.py
+++ activation_compare.py
@@ -8,13 +8,8 @@ from neural_network import Network, Entry, sigmoid, tanh, relu
 # use Polish (system) locale (comma as decimal point, for proper Excel formatting)
 setlocale(LC_ALL, '')
 
-
-# prepare the network
-net = Network([4, 4, 3], 0.02)
-# store the generated net data for comparison
-net_data = net.export_data()
-
-
+funcs = (sigmoid, tanh, relu)
+# read data
 with open("bezdekIris.data", 'r') as file:
     samples: List[Entry] = []
     for row in csv.reader(file):
@@ -29,27 +24,44 @@ with open("bezdekIris.data", 'r') as file:
         samples.append(Entry([float(n) for n in row[0:4]], iris_outputs))
 
 
-def train(activation_f):
-    net.import_data(net_data)  # restore original for comparison purposes
-    net.set_activation_f(activation_f)
-    errors_data = []
-    for i, error in enumerate(net.teach_loop(samples), start=1):
-        if i > 100:
-            break
-        errors_data.append(lstr(error))  # use locale
-    return errors_data
+def train_network():
+    # prepare the network
+    net = Network([4, 4, 3], 0.02)
+    # store the generated net data for comparison
+    net_data = net.export_data()
+
+    def train(activation_f):
+        net.import_data(net_data)  # restore original for comparison purposes
+        net.set_activation_f(activation_f)
+        errors_data = []
+        for i, error in enumerate(net.teach_loop(samples), start=1):
+            if i > number:
+                break
+            errors_data.append(error)
+        return errors_data
+
+    funs_data = []
+    for f in funcs:
+        print(f.__name__)
+        funs_data.append(train(f))
+    return funs_data
 
 
-funcs = (sigmoid, tanh, relu)
-data_funs = []
-
-for f in funcs:
-    print(f.__name__)
-    data_funs.append(train(f))
-
+times = 10
+number = 100
+# repeat for average
+avg_data = train_network()
+for _ in range(times-1):
+    data = train_network()
+    # consolidate
+    for i, new_func_data, avg_func_data in zip(range(len(funcs)), data, avg_data):
+        avg_data[i] = [sum(d) for d in zip(new_func_data, avg_func_data)]
+# divide and convert to polish notation (use locale)
+for i, func_data in enumerate(avg_data):
+    avg_data[i] = [lstr(error / times) for error in func_data]
 # transpose for excel
-data_funs = list(zip(*data_funs))
-
+avg_data = list(zip(*avg_data))
+# save
 with open("neural_data.csv", 'w') as file:
     writer = csv.writer(file, delimiter=';')  # delimit with semicolons
-    writer.writerows(data_funs)
+    writer.writerows(avg_data)
